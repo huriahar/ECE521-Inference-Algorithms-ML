27cce8c730dcb9504bd005c62547f069bb338ba9
MichelleL <user@inside-196-155.wireless.utoronto.ca>
2018-01-21 13:37:04 -0500

    part1 python script

---
+++ A1/part1.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import tensorflow as tf
+import os
+
+def euclideanDistance(x, z):
+    t_x = tf.convert_to_tensor(x)
+    t_z = tf.convert_to_tensor(z)
+    x_d = t_x.shape[-1]
+    z_d = t_x.shape[-1]
+    assert x_d==z_d, "x and z vectors have different dimesions"
+    d = x_d
+    N1 = t_x.shape[0]
+    N2 = t_z.shape[0]
+    t_x_reshaped = tf.reshape(t_x, [N1,1,d])
+    Deuc = tf.square(t_x_reshaped -t_z)
+    Deuc = tf.reduce_sum(Deuc,2)
+    with tf.Session() as session:
+        print(session.run(Deuc))
+
+
+if __name__ == "__main__":
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    x = [[1, 2, 3],[4,5,6]]
+    z = [[-1, -2, -3]]
+    d = euclideanDistance(x, z)
