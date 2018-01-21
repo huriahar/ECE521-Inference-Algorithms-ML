458bda1a511789adf9eee115a86bd022d3749ccf
michelleleiii <michellerp1013@gmail.com>
2018-01-21 14:20:06 -0500

    fix scripts for python2.7 compatibility issue (didn't go through last time)

--- A1/part1.py
+++ A1/part1.py
@@ -2,17 +2,17 @@ from __future__ import print_function
 import tensorflow as tf
 import os
 
-def euclideanDistance(x, z):
-    t_x = tf.convert_to_tensor(x)
-    t_z = tf.convert_to_tensor(z)
-    x_d = t_x.get_shape().as_list()[-1]
-    z_d = t_x.get_shape().as_list()[-1]
+def euclideanDistance(X, Z):
+    t_X = tf.convert_to_tensor(X)
+    t_Z = tf.convert_to_tensor(Z)
+    x_d = t_X.get_shape().as_list()[-1]
+    z_d = t_X.get_shape().as_list()[-1]
     assert x_d==z_d, "x and z vectors have different dimesions"
     d = x_d
-    N1 = t_x.get_shape().as_list()[0]
-    N2 = t_z.get_shape().as_list()[0]
-    t_x_reshaped = tf.reshape(t_x, [N1,1,d])
-    Deuc = tf.square(t_x_reshaped -t_z)
+    N1 = t_X.get_shape().as_list()[0]
+    N2 = t_Z.get_shape().as_list()[0]
+    t_X_reshaped = tf.reshape(t_X, [N1,1,d])
+    Deuc = tf.square(t_X_reshaped - t_Z)
     Deuc = tf.reduce_sum(Deuc,2)
     with tf.Session() as session:
         print(session.run(Deuc))
@@ -20,10 +20,10 @@ def euclideanDistance(x, z):
 
 if __name__ == "__main__":
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    x = [[1, 2, 3],[4,5,6]]
-    z = [[-1, -2, -3]]
-    d = euclideanDistance(x, z)
+    X = [[1, 2, 3],[4,5,6]]
+    Z = [[-1, -2, -3]]
+    d = euclideanDistance(X, Z)
 
-    m = [[1, 2, 3],[4,5,6]]
-    n = [[-1, -2, -3],[1,2,3],[4,5,6]]
-    d = euclideanDistance(m, n)
+    M = [[1, 2, 3],[4,5,6]]
+    N = [[-1, -2, -3],[1,2,3],[4,5,6]]
+    d = euclideanDistance(M, N)
