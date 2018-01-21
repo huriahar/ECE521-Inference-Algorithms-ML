c36a012c65bd43159364f7fbc3a9c72de27347e8
michelleleiii <michellerp1013@gmail.com>
2018-01-21 14:16:45 -0500

    fix scripts for python2.7 compatibility issue

--- A1/part1.py
+++ A1/part1.py
@@ -5,12 +5,12 @@ import os
 def euclideanDistance(x, z):
     t_x = tf.convert_to_tensor(x)
     t_z = tf.convert_to_tensor(z)
-    x_d = t_x.shape[-1]
-    z_d = t_x.shape[-1]
+    x_d = t_x.get_shape().as_list()[-1]
+    z_d = t_x.get_shape().as_list()[-1]
     assert x_d==z_d, "x and z vectors have different dimesions"
     d = x_d
-    N1 = t_x.shape[0]
-    N2 = t_z.shape[0]
+    N1 = t_x.get_shape().as_list()[0]
+    N2 = t_z.get_shape().as_list()[0]
     t_x_reshaped = tf.reshape(t_x, [N1,1,d])
     Deuc = tf.square(t_x_reshaped -t_z)
     Deuc = tf.reduce_sum(Deuc,2)
@@ -23,3 +23,7 @@ if __name__ == "__main__":
     x = [[1, 2, 3],[4,5,6]]
     z = [[-1, -2, -3]]
     d = euclideanDistance(x, z)
+
+    m = [[1, 2, 3],[4,5,6]]
+    n = [[-1, -2, -3],[1,2,3],[4,5,6]]
+    d = euclideanDistance(m, n)
