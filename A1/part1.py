be880958dff05466ebef989917f5136eb426edb6
michelleleiii <michellerp1013@gmail.com>
2018-01-25 14:48:02 -0500

    part2.2 - still missing the plot

--- A1/part1.py
+++ A1/part1.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 def euclideanDistance (X, Z):
     X_shape = X.get_shape().as_list()
@@ -16,17 +17,36 @@ def euclideanDistance (X, Z):
     eucDist = tf.reduce_sum(tf.square(X - Z), axis=2)
     return eucDist
 
-def KNearestNeighbours (distance, k):
+def KNearestNeighbours (distances, row, k):
+    distance = tf.gather(distances, row)
     top_k_vals, top_k_indices = tf.nn.top_k(tf.negative(distance), k=k)
     responsibility = tf.Variable(tf.zeros(shape=distance.get_shape().as_list(), dtype=tf.float32))
-    # updates.shape = indices.shape + ref.shape[1:]
-    updates_shape = top_k_indices.get_shape().as_list() + responsibility.get_shape().as_list()[1:]
+    updates_shape = top_k_indices.get_shape().as_list() + distance.get_shape().as_list()[1:]
     updates = tf.fill(dims=updates_shape, value=tf.to_float(1.0/k))
     responsibility = tf.scatter_update(responsibility, indices=top_k_indices, updates=updates)
     # responsibility is a (dim,) vector -> Reshape to (1, dim)
     dim = responsibility.get_shape().as_list()[0]
-    responsibility = tf.reshape(responsibility, shape=[1, dim])
+    responsibility = tf.reshape(responsibility, shape=[dim, 1])
     return responsibility
+
+def calculate_MSE(X_train, Y_train, X_test, Y_test, K):
+    distances = euclideanDistance(X_test, X_train)
+    N1 = distances.get_shape().as_list()[0]
+    MSE = tf.Variable(0.0, name="mse")
+    for i in range(N1):
+        responsibility = KNearestNeighbours(distances, i, K)
+        Y_head = tf.matmul(tf.transpose(Y_train), responsibility)
+        #print("Y_train: ", Y_train.shape)
+        #print("Y_train transpose: ", tf.transpose(Y_train).shape)
+        #print("responsibility: ", responsibility)
+        #print("Y_head, ", Y_head.shape)
+        #print("Y_test[i], ", Y_test[i].shape)
+        #print("Y_test[i] - Y_head, ", (Y_test[i] - Y_head).shape)
+        err = tf.reduce_sum(tf.square(Y_test[i] - Y_head))
+        MSE += err
+    MSE /= 2*N1
+    return MSE
+    
 
 if __name__ == "__main__":
     np.random.seed(521)
@@ -39,9 +59,14 @@ if __name__ == "__main__":
     testData, testTarget = Data[randIdx[90:100]], Target[randIdx[90:100]]
 
     X_train = tf.placeholder(tf.float32, [80, 1])
-    X_vt = tf.placeholder(tf.float32, [10, 1])
+    X_valid = tf.placeholder(tf.float32, [10, 1])
+    X_test = tf.placeholder(tf.float32, [10, 1])
 
-    feed_dict = {X_train: trainData, X_vt: validData}
+    Y_train = tf.placeholder(tf.float32, [80, 1])
+    Y_valid = tf.placeholder(tf.float32, [10, 1])
+    Y_test = tf.placeholder(tf.float32, [10, 1])
+
+    #feed_dict = {X_train: trainData, X_valid: validData, X_test: testData, Y_train: trainTarget, Y_valid: validTarget, Y_test: testTarget}
 
     X = [[10], [20], [30], [40]]
     Z = [[12], [36]]
@@ -61,27 +86,18 @@ if __name__ == "__main__":
 
     feed_dict2 = {X1_tf:X1, Y1_tf: Y1, Z1_tf: Z1}
 
-    K = 2
-
+    Ks = [1, 3, 5, 50]
     with tf.Session() as sess:
-
         #distance = euclideanDistance(Z_tf, X_tf)
-        distances = euclideanDistance(Z1_tf, X1_tf)     # N2xN1
-        #distance = euclideanDistance(X_vt, X_train)
-
-        #print("distances", distances.get_shape().as_list())
-        #print(sess.run(distances, feed_dict=feed_dict2))
-
-        N2 = distances.get_shape().as_list()[0]
-        for i in range(N2):
-            distance = tf.gather(distances, i)
-            #print("distance", distance.get_shape().as_list())
-            #print(sess.run(distance, feed_dict=feed_dict2))
-            responsibility = KNearestNeighbours(distance, K)
+        #distances = euclideanDistance(Z1_tf, X1_tf)     # N2xN1
+        for K in Ks:
+            print("K=%d:"%K)
+            MSE_train = calculate_MSE(X_train, Y_train, X_train, Y_train, K)
+            MSE_valid = calculate_MSE(X_train, Y_train, X_valid, Y_valid, K)
+            MSE_test = calculate_MSE(X_train, Y_train, X_test, Y_test, K)
             init = tf.global_variables_initializer()
             sess.run(init)
-            #print(sess.run(responsibility, feed_dict=feed_dict2))
-            #print("responsibility", responsibility.get_shape().as_list())
-            #print("Y1_tf", Y1_tf.get_shape().as_list())
-            pred = tf.matmul(responsibility, Y1_tf)
-            #print(sess.run(pred, feed_dict=feed_dict2))
+            print("training MSE loss: ", sess.run(MSE_train, {X_train: trainData, Y_train: trainTarget}))
+            print("valid MSE loss: ", sess.run(MSE_valid, {X_train: trainData, Y_train: trainTarget, X_valid: validData, Y_valid: validTarget}))
+            print("test MSE loss: ", sess.run(MSE_test, {X_train: trainData, Y_train: trainTarget, X_test: testData, Y_test: testTarget}))
+            #sess.run(init)
