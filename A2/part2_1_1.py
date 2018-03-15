d4269ffaa9e174a0fd435184ffaa7e6ec1885375
Padmanabhan  Meghana <meghana.padmanabhan@mail.utoronto.ca>
2018-03-15 17:25:13 -0400

    Adding/Updating files and results of part 2

--- A2/part2_1_1.py
+++ A2/part2_1_1.py
@@ -27,6 +27,20 @@ def loadData(fileName):
         testData, testTarget = Data[3600:], Target[3600:]
         return trainData, trainTarget, validData, validTarget, testData, testTarget
 
+def calculateCrossEntropyLoss(x,y,weights,bias,lambdaParam):
+    logits = (tf.matmul(x,weights) + bias)
+    loss_d = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y,logits=logits))
+    loss_w = (lambdaParam)*(tf.nn.l2_loss(weights))
+    crossEntropyLoss = loss_d + loss_w
+    return crossEntropyLoss
+
+def calculateClassificationAccuracy(x,y,weights,bias):
+    YPred = tf.sigmoid((tf.matmul(x,weights)+bias))
+    YClassfication = tf.cast(tf.greater(YPred , 0.5), tf.float64)
+    YCorrect = tf.cast(tf.equal(YClassfication, y),tf.float64)
+    accuracy = tf.reduce_mean(tf.cast(YCorrect,tf.float64))*100
+    return accuracy
+
 
 if __name__ == "__main__":
     trainData, trainTarget, validData, validTarget, testData, testTarget = loadData("notMNIST.npz")
@@ -54,52 +68,22 @@ if __name__ == "__main__":
     epochs = int(np.ceil(iteration/float(iterPerEpoch)))
     print("Number of epochs=",epochs)
     plt.close('all')
+    
+    #For training set
+    Weights =[]
+    Bias=[]
     Loss = []
-    LossV = []
-    LossTest=[]
-    AccuracyT = []
-    AccuracyV = []
-    AccuracyTest = []
-    best_lr = learnRate[0]
-    minimumLossValid = 1000000.0
+
     for index, lr in enumerate(learnRate):
         w = tf.Variable(tf.truncated_normal([d, 1], stddev=0.1, dtype=tf.float64), name="weights")
         b = tf.Variable(tf.truncated_normal([1], stddev=0.1, dtype=tf.float64), name="biases")
-        #np.random.seed(521)
-
-        #Training Cross Entropy Loss
-        logits = (tf.matmul(XTrain,w) + b)
-        loss_d = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=YTrain,logits=logits))
-        loss_w = (l)*(tf.nn.l2_loss(w))
-        loss = loss_d + loss_w
+        TrainingLoss = [None for ep in range(epochs)]
+        TrainingWeights =[None for ep in range(epochs)]
+        TrainingBias = [None for ep in range(epochs)]
+     
+        loss = calculateCrossEntropyLoss(XTrain,YTrain,w,b,l)
         optimizer = tf.train.GradientDescentOptimizer(lr).minimize(loss)
 
-        #Validation Cross Entropy Loss
-        logitsValid = (tf.matmul(XValid,w) + b)
-        lossValid_d = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=YValid,logits=logitsValid))
-        lossValid_w = (l)*(tf.nn.l2_loss(w))
-        lossValid = lossValid_d + lossValid_w
-
-        #Test Cross Entropy Loss
-        logitsTest = (tf.matmul(XTest,w) + b)
-        lossTest_d = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=YTest,logits=logitsTest))
-        lossTest_w = (l)*(tf.nn.l2_loss(w))
-        lossTest = lossTest_d + lossTest_w
-        
-        #Output Prediction
-        YPred = tf.sigmoid((tf.matmul(X,w)+b))
-        YClassfication = tf.cast(tf.greater(YPred , 0.5), tf.float64)
-        YCorrect = tf.cast(tf.equal(YClassfication, Y),tf.float64)
-        accuracy = tf.reduce_mean(tf.cast(YCorrect,tf.float64))*100
-
-        fig = plt.figure(index*2+1)
-     
-        TrainingLoss = [None for ep in range(epochs)]
-        TrainingAccuracy = [None for ep in range(epochs)]
-        ValidLoss = [None for ep in range(epochs)]
-        ValidAccuracy = [None for ep in range(epochs)]
-        TestLoss = [None for ep in range(epochs)]
-        TestAccuracy = [None for ep in range(epochs)]
         init = tf.global_variables_initializer()
         sess.run(init)
         for ep in range(epochs):
@@ -110,66 +94,63 @@ if __name__ == "__main__":
                 else:
                     XBatch = trainData[iteration*batchSize :(iteration+1)*batchSize]
                     YBatch = trainTarget[iteration*batchSize:(iteration+1)*batchSize]
-                _,TrainingLoss[ep] = sess.run([optimizer,loss],feed_dict= {XTrain:XBatch,YTrain:YBatch})
-            ValidLoss[ep] = sess.run(lossValid,feed_dict={XValid:validData, YValid:validTarget})
-            TestLoss[ep] = sess.run(lossTest, feed_dict={XTest:testData,YTest:testTarget})
-            TrainingAccuracy[ep]= sess.run(accuracy,feed_dict={X:trainData, Y:trainTarget})
-            ValidAccuracy[ep]= sess.run(accuracy,feed_dict={X:validData, Y:validTarget})
-            TestAccuracy[ep]= sess.run(accuracy,feed_dict={X:testData, Y:testTarget})
-
-        # To Do: improve plots!!!!!!!
-        plt.scatter(range(epochs), TrainingLoss, marker='.',)
-        plt.scatter(range(epochs), TrainingAccuracy, marker='.',)
-        plt.scatter(range(epochs), ValidLoss, marker='*',)
-        plt.scatter(range(epochs), ValidAccuracy, marker='*',)
-        plt.xlabel('the n-th epoch')
-        plt.ylabel('loss/Accuracy')
-        plt.title("MSE vs number of epoch for learning rate of %f" % lr)
-        fig.savefig("part2_1_learnrate_%d.png"%index)
-        fig = plt.figure(index * 2 + 2)
-        plt.scatter(range(100,epochs), TrainingLoss[100:], marker='.', )
-        plt.xlabel('the n-th epoch')
-        plt.ylabel('loss')
-        plt.title("MSE vs number of epoch for learning rate of %f" % lr)
-        fig.savefig("part2_1_learnrate_%d_zoomedin.png" % index)
+                _,TrainingLoss[ep], TrainingWeights[ep],TrainingBias[ep]= sess.run([optimizer,loss,w,b],feed_dict= {XTrain:XBatch,YTrain:YBatch})
         
-        #appending the losses and accuracies across epochs for training, validation and test
         Loss.append(TrainingLoss)
-        LossV.append(ValidLoss)
-        LossTest.append(TestLoss)
-        AccuracyT.append(TrainingAccuracy)
-        AccuracyV.append(ValidAccuracy)
-        AccuracyTest.append(TestAccuracy)
+        Weights.append(TrainingWeights)
+        Bias.append(TrainingBias)
 
-    print("Validation Loss")
-    print("Learning rate 0.005",LossV[0][epochs-1])
-    print("Learning rate 0.001",LossV[1][epochs-1])
-    print("Learning rate 0.0001",LossV[2][epochs-1])
 
-    print("Training Accuracy")
-    print("Lerning rate 0.005",AccuracyT[0][epochs-1])
-    print("Learning rate 0.001",AccuracyT[1][epochs-1])
-    print("Learning rate 0.0001",AccuracyT[2][epochs-1])
+    #Select the best learning rate based on training loss
+    best_lr = learnRate[0]
+    bestLrIndex = 0
+    minimumTrainingLoss= float('inf')
+    for index,lr in enumerate(learnRate):
+        if(Loss[index][-1] <minimumTrainingLoss):
+            minimumTrainingLoss = Loss[index][-1]
+            best_lr = learnRate[index]
+            bestLrIndex = index
 
-    print("Validation Accuracy")
-    print("Lerning rate 0.005",AccuracyV[0][epochs-1])
-    print("Learning rate 0.001",AccuracyV[1][epochs-1])
-    print("Learning rate 0.0001",AccuracyV[2][epochs-1])
-   
-    print("Test Accuracy")
-    print("Lerning rate 0.005",AccuracyTest[0][epochs-1])
-    print("Learning rate 0.001",AccuracyTest[1][epochs-1])
-    print("Learning rate 0.0001",AccuracyTest[2][epochs-1])
+    #Calculating training accuracy, validation loss and validation accuracy for best learning rate
+    TrainingAccuracy = [None for ep in range(epochs)]
+    ValidLoss = [None for ep in range(epochs)]
+    ValidAccuracy = [None for ep in range(epochs)]
+    TestAccuracy = [None for ep in range(epochs)]
+    for ep in range(epochs):
+        weightEp = Weights[bestLrIndex][ep]
+        biasEp =Bias[bestLrIndex][ep]
 
-    #To Do: improve plots for all learning rates in one plot
-    fig2 = plt.figure(7)
+        #accuracy
+        accuracy = calculateClassificationAccuracy(X,Y,weightEp,biasEp)
+
+        #Validation Loss
+        validationLoss = calculateCrossEntropyLoss(XValid, YValid, weightEp, biasEp,l)
+
+        TrainingAccuracy[ep]= sess.run(accuracy, feed_dict={X:trainData,Y:trainTarget})
+        ValidLoss[ep] = sess.run(validationLoss,feed_dict={XValid: validData, YValid: validTarget})
+        ValidAccuracy[ep] = sess.run(accuracy,feed_dict={X:validData, Y:validTarget})
+        TestAccuracy[ep] = sess.run(accuracy,feed_dict={X:testData, Y:testTarget})
+
+
+    plt.close('all')
+    fig = plt.figure(1)
+    plt.scatter(range(epochs), Loss[bestLrIndex], marker='.', c='r', label='training')
+    plt.scatter(range(epochs), ValidLoss, marker='.', c='b', label='validation')
     plt.xlabel('the n-th epoch')
-    plt.ylabel('loss')
-    plt.scatter(range(epochs), Loss[0],marker='*', c='r')
-    plt.scatter(range(epochs), Loss[1], marker='*', c='b')
-    plt.scatter(range(epochs), Loss[2],marker='*', c='g')
-    plt.scatter(range(epochs), LossV[0],marker='.', c='r')
-    plt.scatter(range(epochs), LossV[1], marker='.', c='b')
-    plt.scatter(range(epochs), LossV[2],marker='.', c='g')
-    fig2.savefig("part2_1_all_learning_rates")
+    plt.ylabel('cross-entropy loss')
+    plt.legend()
+    plt.title("cross-entropy loss vs number of epochs for learning rate of %f" % learnRate[bestLrIndex])
+    fig.savefig("part2_1_1_loss.png")
+    fig = plt.figure(2)
+    plt.scatter(range(epochs), TrainingAccuracy, marker='.', c='r', label='training')
+    plt.scatter(range(epochs), ValidAccuracy, marker='.', c='b', label='validation')
+    plt.xlabel('the n-th epoch')
+    plt.ylabel('accuracy')
+    plt.legend()
+    plt.title("accuracy vs number of epochs for learning rate of %f" % learnRate[bestLrIndex])
+    fig.savefig("part2_1_1_accuracy.png")
+    print("Best Learning Rate: %f" % learnRate[bestLrIndex])
+    print("Classification accuracy on training data: %f" % TrainingAccuracy[-1])
+    print("Classification accuracy on validation data: %f" % ValidAccuracy[-1])
+  
 
