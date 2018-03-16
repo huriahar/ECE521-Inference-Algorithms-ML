Fixing a few bugs in MSE loss calculation

A2/part1_1.py
@@ -23,7 +23,16 @@ def loadData (fileName):
         trainData, trainTarget = Data[:3500], Target[:3500]
         validData, validTarget = Data[3500:3600], Target[3500:3600]
         testData, testTarget = Data[3600:], Target[3600:]
-        return trainData, trainTarget, validData, validTarget, testData, testTarget    
+        return trainData, trainTarget, validData, validTarget, testData, testTarget
+
+def calculateMSELoss(X, Y, w, b, lda):
+    YHead = tf.matmul(X, w) + b
+    loss = tf.reduce_sum(tf.squared_difference(YHead, Y))
+    N = X.get_shape().as_list()[0]
+    loss = tf.divide(loss, tf.to_double(2*N))
+    regularizer = tf.nn.l2_loss(w)
+    loss = loss + lda*regularizer
+    return loss
 
 if __name__ == "__main__":
     trainData, trainTarget, validData, validTarget, testData, testTarget = loadData("notMNIST.npz")
@@ -43,17 +52,15 @@ if __name__ == "__main__":
     YTest = tf.placeholder(tf.float64, testTarget.shape)
 
     iteration = 20000.
-    w = tf.Variable(tf.truncated_normal([d, 1], stddev=0.5, dtype=tf.float64), name="weights")
+    w = tf.Variable(tf.truncated_normal([d, 1], stddev=0.5, seed=521, dtype=tf.float64), name="weights")
     b = tf.Variable(0.0, dtype=tf.float64, name="biases")
-    YHead = tf.matmul(XTrain, w) + b                     # (500, 1)
-    N = len(trainData)                                   # 3500
-    loss = tf.reduce_sum(tf.squared_difference(YHead, YTrain))
-    loss = tf.divide(loss,tf.to_double(2*N))
-    losses = []
-    regularizer = tf.nn.l2_loss(w)
-    lda = 0.0       # lambda i.e. weight decay coefficient
-    loss = loss + lda * regularizer
 
+    lda = 0.0       # lambda i.e. weight decay coefficient
+    loss = calculateMSELoss(XTrain, YTrain, w, b, lda)
+
+    N = len(trainData)                                   # 3500
+    losses = []
+    
     init = tf.global_variables_initializer()
     sess.run(init)
     learnRate = [0.005, 0.001, 0.0001]
@@ -72,7 +79,7 @@ if __name__ == "__main__":
                 feed = {XTrain:XBatch, YTrain:YBatch}
                 _,  L[ep] = sess.run([optimizer, loss], feed_dict=feed)
 
-        print("Minimum loss for learning rate", lr, "is:", min(L))
+        print("Minimum loss for learning rate", lr, "is:", L[-1])
         losses.append(L)
         plt.scatter(range(epochs), L, marker='|')
         plt.xlabel('the n-th epoch')
@@ -91,11 +98,13 @@ if __name__ == "__main__":
 
     #####################
     fig = plt.figure((index+1)*2 + 1)
-    plt.scatter(range(epochs), losses[0], marker='|', c='r', label='n = %f'%learnRate[0])
-    plt.scatter(range(epochs), losses[1], marker='|', c='g', label='n = %f'%learnRate[1])
-    plt.scatter(range(epochs), losses[2], marker='|', c='b', label='n = %f'%learnRate[2])
+    plt.plot(range(epochs), losses[0], c='r', label='n = %f'%learnRate[0])
+    plt.plot(range(epochs), losses[1], c='g', label='n = %f'%learnRate[1])
+    plt.plot(range(epochs), losses[2], c='b', label='n = %f'%learnRate[2])
     plt.legend()
-    plt.title("MSE vs number of epoch for different learning rates")
+    plt.title("MSE vs number of epochs for different learning rates")
+    plt.xlabel("Number of epochs")
+    plt.ylabel("Men Squared Error")
     fig.savefig("part1_1_AllInOne.png")
 
     #print(sess.run(YTrain,{YTrain:trainTarget}))

A2/part1_2.py
@@ -25,6 +25,15 @@ def loadData(fileName):
         testData, testTarget = Data[3600:], Target[3600:]
         return trainData, trainTarget, validData, validTarget, testData, testTarget
 
+def calculateMSELoss(X, Y, w, b, lda):
+    YHead = tf.matmul(X, w) + b
+    loss = tf.reduce_sum(tf.squared_difference(YHead, Y))
+    N = X.get_shape().as_list()[0]
+    loss = tf.divide(loss, tf.to_double(2*N))
+    regularizer = tf.nn.l2_loss(w)
+    loss = loss + lda*regularizer
+    return loss
+
 if __name__ == "__main__":
     trainData, trainTarget, validData, validTarget, testData, testTarget = loadData("notMNIST.npz")
 
@@ -35,7 +44,7 @@ if __name__ == "__main__":
     # chosen from part1.1
     learnRate = 0.005
     lda = 0.0
-    N = len(trainData)
+    N = len(trainData)              # 3500
 
     XValid = tf.placeholder(tf.float64, validData.shape)
     YValid = tf.placeholder(tf.float64, validTarget.shape)
@@ -50,13 +59,9 @@ if __name__ == "__main__":
         iterPerEpoch = int(np.ceil(float(N) / batchSize))
         epochs = int(np.ceil(iteration / float(iterPerEpoch)))
 
-        w = tf.Variable(tf.truncated_normal([d, 1], stddev=0.5, dtype=tf.float64), name="weights")
+        w = tf.Variable(tf.truncated_normal([d, 1], stddev=0.5, seed=521, dtype=tf.float64), name="weights")
         b = tf.Variable(0.0, dtype=tf.float64, name="biases")
-        YHead = tf.matmul(XTrain, w) + b
-        loss = tf.reduce_sum(tf.squared_difference(YHead, YTrain))
-        loss = tf.divide(loss, tf.to_double(2 * N))
-        regularizer = tf.nn.l2_loss(w)
-        loss = loss + lda * regularizer
+        loss = calculateMSELoss(XTrain, YTrain, w, b, lda)
         init = tf.global_variables_initializer()
         sess.run(init)
         optimizer = tf.train.GradientDescentOptimizer(learnRate).minimize(loss)

A2/part1_3and4.py
@@ -25,6 +25,25 @@ def loadData(fileName):
         testData, testTarget = Data[3600:], Target[3600:]
         return trainData, trainTarget, validData, validTarget, testData, testTarget
 
+def calculateMSELoss(X, Y, w, b, lda):
+    YHead = tf.matmul(X, w) + b
+    loss = tf.reduce_sum(tf.squared_difference(YHead, Y))
+    N = X.get_shape().as_list()[0]
+    loss = tf.divide(loss, tf.to_double(2*N))
+    regularizer = tf.nn.l2_loss(w)
+    loss = loss + lda*regularizer
+    return loss
+
+def calculateAccuracy(X, Y, w, b):
+    YHead = tf.matmul(X, w) + b
+    # Creates a vector with each entry being either True/False
+    # Entry is True when value in YHead < 0.5
+    lessThanHalf = tf.less(YHead, tf.constant(0.5, shape=YHead.shape, dtype=tf.float64))
+    # Classification value is 0 if True else 1
+    YHeadClassified = tf.where(lessThanHalf, tf.zeros(tf.shape(YHead), dtype=tf.float64), tf.ones(tf.shape(YHead), dtype=tf.float64))
+    accuracy, updateOp = tf.metrics.accuracy(labels=Y, predictions=YHeadClassified)
+    return accuracy, updateOp
+
 if __name__ == "__main__":
     trainData, trainTarget, validData, validTarget, testData, testTarget = loadData("notMNIST.npz")
 
@@ -35,12 +54,13 @@ if __name__ == "__main__":
     # chosen from part1.1
     learnRate = 0.005
     ldas = [0., 0.001, 0.1, 1.]
-    best_ldas = ldas[0]
-    min_lossValid = float('inf')
+    bestLda = ldas[0]
+    maxValidAccuracy = 0
     N = len(trainData)
-    NValid = len(validData)
     iterPerEpoch = int(N / batchSize)                       # 7
     epochs = int(np.ceil(iteration / float(iterPerEpoch)))  # 2858
+
+    bestWeight = bestBias = None
 
     XTrain = tf.placeholder(tf.float64, [batchSize, d])
     YTrain = tf.placeholder(tf.float64, [batchSize, 1])
@@ -54,11 +74,7 @@ if __name__ == "__main__":
     for lda in ldas:
         w = tf.Variable(tf.truncated_normal([d, 1], stddev=0.5, seed=521, dtype=tf.float64), name="weights")
         b = tf.Variable(0.0, dtype=tf.float64, name="biases")
-        YHead = tf.matmul(XTrain, w) + b
-        loss = tf.reduce_sum(tf.squared_difference(YHead, YTrain))
-        loss = tf.divide(loss, tf.to_double(2 * N))
-        regularizer = tf.nn.l2_loss(w)
-        loss = loss + lda * regularizer
+        loss = calculateMSELoss(XTrain, YTrain, w, b, lda)
         init = tf.global_variables_initializer()
         sess.run(init)
         optimizer = tf.train.GradientDescentOptimizer(learnRate).minimize(loss)
@@ -72,29 +88,33 @@ if __name__ == "__main__":
                 feed = {XTrain: XBatch, YTrain: YBatch}
                 _, L[ep] = sess.run([optimizer, loss], feed_dict=feed)
         end = time.time()
-        YHeadValid = tf.matmul(XValid, w) + b
-        lossValid = tf.reduce_sum(tf.squared_difference(YHeadValid, YValid))
-        lossValid = tf.divide(lossValid, tf.to_double(2 * NValid))
-        cond = tf.less(YHeadValid, tf.constant(0.5, shape=YHeadValid.shape, dtype=tf.float64))
-        YHeadClassifiedValid = tf.where(cond, tf.zeros(tf.shape(YHeadValid), dtype=tf.float64), tf.ones(tf.shape(YHeadValid), dtype=tf.float64))
-        accuracy, update_op = tf.metrics.accuracy(labels=YValid, predictions=YHeadClassifiedValid)
+        validLoss = calculateMSELoss(XValid, YValid, w, b, lda)
+        accuracy, updateOp = calculateAccuracy(XValid, YValid, w, b)
         tf.local_variables_initializer().run()
-        _ , o, lossValid = sess.run([accuracy, update_op, lossValid], feed_dict={XValid:validData, YValid:validTarget})
+        validLoss, _, updateOp = sess.run([validLoss, accuracy, updateOp], feed_dict={XValid:validData, YValid:validTarget})
         accuracy = sess.run(accuracy)
-        print("With lambda=%f, MSE in validation set: %f, classification accuracy in validation set: %f, computation time: %f seconds " % (lda, lossValid, accuracy, end-start))
-        if (lossValid < min_lossValid):
-            best_ldas = lda
-            min_lossValid = lossValid
+        print("With lambda=%f, MSE in validation set: %f, classification accuracy in validation set: %f, computation time: %f seconds " % (lda, validLoss, accuracy, end-start))
+        if (accuracy > maxValidAccuracy):
+            bestLda = lda
+            maxValidAccuracy = accuracy
+            bestWeight = w
+            bestBias = b
 
-    # TODO: Add test accurcy part
+    print("Best lambda based on highest validation accuracy:", bestLda)
+
+    # Test Accuracy
+    testAccuracy, testUpdateOp = calculateAccuracy(XTest, YTest, bestWeight, bestBias)
+    tf.local_variables_initializer().run()
+    _, testUpdateOp = sess.run([testAccuracy, testUpdateOp], feed_dict={XTest:testData, YTest:testTarget})
+    testAccuracy = sess.run(testAccuracy)
+    print("Test Accuracy with the selected weight decay coefficient: %f" % testAccuracy)
 
     #####################
     #part1.4
     #normal equation:
     #wLS = (XT*X)-1 * XT * Y
     XTrain = tf.placeholder(tf.float64, [len(trainData), d])
-    shape = XTrain.get_shape()
-    shape = shape.as_list()
+    shape = XTrain.get_shape().as_list()
     shape[-1] = 1
     one = tf.ones(shape, dtype=tf.float64)
     XExtended = tf.concat([one, XTrain], 1)
@@ -106,8 +126,8 @@ if __name__ == "__main__":
     start = time.time()
     sess.run(wLS, feed_dict={XTrain:trainData, YTrain:trainTarget})
     end = time.time()
-    shape = XValid.get_shape()
-    shape = shape.as_list()
+    
+    shape = XValid.get_shape().as_list()
     shape[-1] = 1
     one = tf.ones(shape, dtype=tf.float64)
     XValidExtended = tf.concat([one, XValid], 1)
@@ -122,5 +142,3 @@ if __name__ == "__main__":
     accuracyNormalEq = tf.to_double(tf.reduce_sum(accuracyNormalEq)) / tf.to_double(tf.size(accuracyNormalEq))
     accuracyNormalEq, lossNormalEq = sess.run([accuracyNormalEq, lossNormalEq], feed_dict={XValid:validData, YValid:validTarget, XTrain:trainData, YTrain:trainTarget})
     print("Using the normal equation, MSE in validation set: %f, classification accuracy in validation set: %f, computation time: %f seconds " % (lossNormalEq, accuracyNormalEq, end-start))
-
-